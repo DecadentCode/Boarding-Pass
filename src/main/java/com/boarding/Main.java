23b08f4a1afd6d83dbef4321eaf99c19a7262ee9
Andrew Cyburt <newswithdrew@gmail.com>
2022-05-03 16:40:23 -0400

    Initial UI branch commit

--- src/main/java/com/boarding/Main.java
+++ src/main/java/com/boarding/Main.java
@@ -1,6 +1,8 @@
 package com.boarding;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,7 +10,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Main extends Application {
+public class Main extends Application{
     public static void main(String[] args) {
         launch(args);
     }
@@ -29,8 +31,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 412, 732);
+        stage.setTitle("Air-Con Boarding");
         stage.setScene(scene);
         stage.show();
     }
