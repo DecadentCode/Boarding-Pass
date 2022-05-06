4e3732129a0622f62500f641572a19281fa27abd
Andrew Cyburt <newswithdrew@gmail.com>
2022-05-06 16:42:43 -0400

    Revert "Merge branch 'master' into Andrew/UI"
    This reverts commit 08e121dd562281923c4d41594c2d61fd3fc067f1, reversing
    changes made to 09c5f80ce181ac064946032f18452d912b785ba8.

--- src/main/java/com/boarding/BoardingPass.java
+++ src/main/java/com/boarding/BoardingPass.java
@@ -1,29 +1,13 @@
 package com.boarding;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.util.EnumMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.UUID;
 
 public class BoardingPass {
@@ -59,6 +43,7 @@ public class BoardingPass {
     {
        return BPNumber;
     }
+
     public void serialize() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
@@ -69,64 +54,12 @@ public class BoardingPass {
             BufferedWriter out = new BufferedWriter(file);
             out.write(boardingPass);
             out.close();
-            Map<EncodeHintType, Object> hintType = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
-            hintType.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-            hintType.put(EncodeHintType.MARGIN, 1);
-            hintType.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
-            QRCodeWriter qrCodeWriter = new QRCodeWriter();
-            BitMatrix bitMatrix = qrCodeWriter.encode(boardingPass, BarcodeFormat.QR_CODE, 256,
-                    256, hintType);
-            BufferedImage qrImage = new BufferedImage(bitMatrix.getWidth(), bitMatrix.getWidth(),
-                    BufferedImage.TYPE_INT_RGB);
-            qrImage.createGraphics();
-            Graphics2D qrGraphics = (Graphics2D) qrImage.getGraphics();
-            qrGraphics.setColor(Color.white);
-            qrGraphics.fillRect(0, 0, bitMatrix.getWidth(), bitMatrix.getWidth());
-            qrGraphics.setColor(Color.black);
-            for (int i = 0; i < bitMatrix.getWidth(); i++) {
-                for (int j = 0; j < bitMatrix.getWidth(); j++) {
-                    if (bitMatrix.get(i, j)) {
-                        qrGraphics.fillRect(i, j, 1, 1);
-                    }
-                }
-            }
-            ImageIO.write(qrImage, "png", new File("target/bp_qr.png"));
-            File jsonFile = new File("target/boarding_pass.json");
-            Scanner scanner = new Scanner(jsonFile);
-            StringBuilder sb = new StringBuilder();
-            while(scanner.hasNextLine()) {
-                sb.append(scanner.nextLine().replace(": {", " :\n")
-                        .replace("}", "").replace("\"", "")
-                        .replace(",", "\n").replace("{" , ""));
-            }
-            PdfWriter pdfWriter = new PdfWriter("target/boarding_pass.pdf");
-            PdfDocument pdf = new PdfDocument(pdfWriter);
-            pdf.addNewPage();
-            Document document = new Document(pdf);
-            document.close();
-            PDDocument doc = PDDocument.load(new File("target/boarding_pass.pdf"));
-            PDPage page = doc.getPage(0);
-            PDPageContentStream contentStream = new PDPageContentStream(doc, page);
-            contentStream.beginText();
-            contentStream.moveTextPositionByAmount(10, 800);
-            contentStream.setFont(PDType1Font.HELVETICA, 16);
-            for (String s : sb.toString().split("\n")) {
-                int y = -16;
-                contentStream.showText(s);
-                contentStream.newLineAtOffset(0, y);
-            }
-            contentStream.endText();
-            contentStream.close();
-            doc.save(new File("target/boarding_pass.pdf"));
-            doc.close();
+
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            System.out.println("IOException occurred during serialization!\n\n");
-            e.printStackTrace();
-        } catch (WriterException e) {
-            System.out.println("WriterException occurred during serialization!\n\n");
-            e.printStackTrace();
+            System.out.println("IOException occurred during serialization!\n");
         }
+
     }
 }
