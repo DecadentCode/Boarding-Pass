852211d1cd9c7e296df66eb2fbe421e9ac865fee
Trystan Meadows <trystan.meadows@genspark.net>
2022-05-07 18:25:04 -0500

    ETA is finished, tested, works

--- src/main/java/com/boarding/Flight.java
+++ src/main/java/com/boarding/Flight.java
@@ -1,10 +1,10 @@
 package com.boarding;
 
 import java.io.Serializable;
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.LocalDate;
-import java.util.Arrays;
 
 public class Flight implements Serializable {
 
@@ -70,8 +70,9 @@ public class Flight implements Serializable {
 
     public LocalTime getETA()
     {
-        double timeOfArrival = getETA(origin, destination);
-        return LocalTime.of(12, 30);
+        LocalDateTime timeOfArrival = getETA(origin, destination, DT, date);
+        this.ETA = LocalTime.of(timeOfArrival.getHour(), timeOfArrival.getMinute());
+        return this.ETA;
     }
 
     public LocalTime getDT()
@@ -80,6 +81,8 @@ public class Flight implements Serializable {
     }
 
 
+
+    // ETA Calculations
     public static double getDistance(double x1, double x2, double y1, double y2)
     {
         final int R = 3959; //earth's mean radius in miles
@@ -97,9 +100,8 @@ public class Flight implements Serializable {
         return value * Math.PI / 180;
     }
 
-    //Takes an origin and destionation as a String and returns the ETA
-
-    private static double getETA( String origin, String destination)
+    //Takes an origin and destination as a String and returns the ETA
+    private static LocalDateTime getETA( String origin, String destination, LocalTime DT, LocalDate date)
     {
         Locations dictionary = new Locations();
         Location one = dictionary.getLocation(origin);
@@ -107,9 +109,31 @@ public class Flight implements Serializable {
         final double avgSpeed = 500;
         double distance = getDistance(one.getLongitude(), two.getLongitude(), one.getLatitude(), two.getLatitude());
         double ETA = (distance / avgSpeed) * 2;
-        String[] strArr = String.valueOf(ETA).replace(".", ",").split(" ");
-        System.out.println(Arrays.toString(strArr));
-        return Math.round(ETA);
+        String[] strArr = String.valueOf(new DecimalFormat("##.##").format(ETA))
+                .replace(".", ",").split(",");
+        int hours = Integer.parseInt(strArr[0]);
+        int mins = Integer.parseInt(strArr[1]);
+
+        LocalTime LT2 = DT.plusHours(hours).plusMinutes(mins);
+
+        if(LT2.getHour() >= 23)
+        {
+            LocalTime LT3 = LT2.minusHours(LT2.getHour()).minusMinutes(LT2.getMinute());
+            LocalDateTime TotalTravelTime = LocalDateTime.of(date.plusDays(1), LT3);
+            return TotalTravelTime;
+        }
+        else if (DT.getMinute() >= 60)
+        {
+            LocalTime LT3 = LocalTime.of(LT2.getHour() + 1, 00);
+            LocalDateTime TotalTravelTime = LocalDateTime.of(date, LT3);
+            return TotalTravelTime;
+        }
+        else
+        {
+            LocalDateTime TotalTravelTime = LocalDateTime.of(date, LT2);
+            return TotalTravelTime;
+        }
+
     }
 }
 
